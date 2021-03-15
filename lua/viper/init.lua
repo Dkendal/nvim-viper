3da2a0a10be8cccfa6c6487010ae96c409077f3e
Dylan Kendal <dylankendal@gmail.com>
2021-03-14 21:05:44 -0400

    Add ViperGrep

--- lua/viper/init.lua
+++ lua/viper/init.lua
@@ -1,5 +1,6 @@
 local fzf = require 'fzf'
 local a = require 'viper.async'
+
 local async, await, main = a.sync, a.wait, a.main
 local fn, api, cmd = vim.fn, vim.api, vim.cmd
 local format = string.format
@@ -8,6 +9,11 @@ local Mod = {}
 local registry = {}
 
 local fzf_opts = { ansi = true, expect = { 'ctrl-c', 'ctrl-g', 'ctrl-d', 'enter' } }
+
+local function log_error(value)
+  dump('ERROR:', value)
+  api.nvim_err_writeln(vim.inspect(value))
+end
 
 -- @alias Buffer number
 
@@ -35,6 +41,7 @@ local function inspect(...)
   print(unpack(vim.tbl_map(vim.inspect, { ... })))
 end
 
+-- Replace termcodes
 local function t(keys)
   return api.nvim_replace_termcodes(keys, true, true, true)
 end
@@ -48,6 +55,7 @@ end
 -- Run a function with the current buffer and win, and restore back to them
 -- after the function completes.
 local function with_current_buf_win(func)
+  local view = fn.winsaveview()
   local buf = api.nvim_get_current_buf()
   local win = api.nvim_get_current_win()
 
@@ -55,6 +63,7 @@ local function with_current_buf_win(func)
 
   api.nvim_set_current_win(win)
   api.nvim_set_current_buf(buf)
+  fn.winrestview(view)
 
   if not success then
     error(result)
@@ -74,7 +83,7 @@ local function with_temp_buf(func)
     local success, result = pcall(func)
 
     if api.nvim_buf_is_valid(buf) then
-      api.nvim_buf_delete(buf)
+      api.nvim_buf_delete(buf, {})
     end
 
     if not success then
@@ -98,15 +107,14 @@ end
 
 -- return the current line in the fzf selection
 local function fzf_current_line(lines)
-  local match_count = '(%d)/%d'
-  local current = '>%s+(.*)'
-
   for _, value in ipairs(lines) do
-    local curr_match = string.match(value, current)
-
-    if string.match(value, match_count) then
-      break
+    -- The number of matches line if this is the current line there aren't any
+    -- valid matches
+    if value:match('(%d)/%d') then
+      return nil
     end
+
+    local curr_match = value:match('>%s+(.*)')
 
     if curr_match then
       return curr_match
@@ -126,6 +134,38 @@ local function buf_set_keymap(buffer, mode, lhs, rhs, opts)
   registry[buffer][mode][t_lhs] = rhs
   rhs = format([[<CMD>lua require("viper").buf_call(%d, "%s", "%s", {})<CR>]], buffer, mode, t_lhs)
   api.nvim_buf_set_keymap(buffer, mode, lhs, rhs, opts)
+end
+
+-- Register an event handler for when the selected line changes in the fzf window.
+local function on_selection_change(callback)
+  local on_lines = function(_, bufnr, _, firstline, _, new_lastline, _, _, _)
+    if (new_lastline - firstline) == 1 then
+      -- Don't do anything for now when it's just the last line
+      return
+    end
+
+    local status, value = pcall(function()
+      local lines = api.nvim_buf_get_lines(bufnr, firstline, new_lastline, false)
+      local line = fzf_current_line(lines)
+
+      if line and line:len() > 0 then
+        callback(line)
+      end
+    end)
+
+    if not status then
+      log_error(value)
+    end
+  end
+
+  api.nvim_buf_attach(0, false, { on_lines = on_lines })
+end
+
+local function parse_vimgrep(text)
+  local file, line, col = text:match('^(.+):(%d+):(%d+):')
+  line = tonumber(line)
+  col = tonumber(col)
+  return file, line, col
 end
 
 function Mod.buffers()
@@ -197,13 +237,13 @@ function Mod.registers()
   end)()
 end
 
-function Mod.files()
+-- @param source string Shell command that returns a file path
+function Mod.files(source)
   async(function()
     await(main)
 
     local choice = with_temp_buf(function()
       map_esc_to_ctrl_c()
-      local source = 'fd'
       return fzf.provided_win_fzf(source, merge_fzf_opts({}))
     end)
 
@@ -215,11 +255,63 @@ function Mod.files()
   end)()
 end
 
+-- @param source string Shell command that returns vimgrep formatted results
+function Mod.grep(source)
+  local win = api.nvim_get_current_win()
+
+  async(function()
+    await(main)
+
+    local ns = api.nvim_create_namespace('Viper Grep')
+    local hl_group = 'Search'
+
+    local choice = with_temp_buf(function()
+      map_esc_to_ctrl_c()
+
+      on_selection_change(function(text)
+        local file, line, col = parse_vimgrep(text)
+        local new_buf = fn.bufexists(file) == 0
+        local buf = fn.bufadd(file)
+
+        main(function()
+          api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+          api.nvim_buf_add_highlight(buf, ns, hl_group, line - 1, 0, -1)
+          api.nvim_win_set_buf(win, buf)
+
+          api.nvim_buf_call(buf, function()
+            fn.setpos('.', { buf, line, col })
+
+            -- center the buffer in the window around the selection
+            cmd 'keepjumps normal zz'
+
+            if new_buf then
+              cmd 'filetype detect'
+            end
+          end)
+        end)
+      end)
+
+      return fzf.provided_win_fzf(source, merge_fzf_opts({}))
+    end)
+
+    api.nvim_buf_clear_namespace(0, ns, 0, -1)
+
+    local key, selection = unpack(choice)
+
+    local file, line = parse_vimgrep(selection)
+
+    if key == 'enter' then
+      cmd(format('e +%d %s', line, file))
+    end
+  end)()
+end
+
 function Mod.init()
   api.nvim_exec([[
   command! ViperBuffers :lua require("viper").buffers()
   command! ViperRegisters :lua require("viper").registers()
-  command! ViperFiles :lua require("viper").files()
+  command! -nargs=* ViperFiles :lua require("viper").files(<q-args>)
+  command! -nargs=* ViperGrep :lua require("viper").grep(<q-args>)
   ]], false)
 end
 
